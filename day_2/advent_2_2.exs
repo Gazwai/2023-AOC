6fbdd1090a70dc618845e0b2b53b6a9a03692a8a
Gary Y <garyyau080@gmail.com>
2023-12-13 01:00:27 +0900

    Refactor: Remove Day 1 code

--- day_2/advent_2_2.exs
+++ day_2/advent_2_2.exs
@@ -1,98 +1,3 @@
-# defmodule Helper do
-#   @doc """
-#   Parses a string containing a number and a color and returns a tuple with the number (as integer) and the color.
-
-#   ## Examples
-
-#       iex> Helper.parse_number_and_color("12 red")
-#       {12, "red"}
-#   """
-#   def parse_number_and_color(str) do
-#     [num_str, color] = Regex.scan(~r/\d+|[a-z]+/, str) |> List.flatten()
-#     {String.to_integer(num_str), color}
-#   end
-
-#   @doc """
-#   Returns the maximum value for a specified color within a list of {number, color} tuples.
-
-#   ## Examples
-
-#       iex> Helper.max_color_value([{12, "red"}, {7, "blue"}, {10, "red"}], "red")
-#       12
-#   """
-#   def max_color_value(game, color) do
-#     game
-#     |> Enum.filter(fn {_, c} -> c == color end)
-#     |> Enum.map(&elem(&1, 0))
-#     |> Enum.max()
-#   end
-# end
-
-# defmodule GameProcessor do
-#   alias Helper
-
-#   @doc """
-#   Processes game data from a file. Reads the file, splits the content into games, and processes each game.
-
-#   ## Examples
-
-#       iex> GameProcessor.process_game_data("path/to/file.txt")
-#       Total Score: 123
-#   """
-#   def process_game_data(file_path) do
-#     case File.read(file_path) do
-#       {:ok, content} ->
-#         process_content(content)
-
-#       {:error, reason} ->
-#         IO.inspect("Failed to read the file: #{reason}")
-#     end
-#   end
-
-#   @doc false
-#   defp process_content(content) do
-#     content
-#     |> String.trim()
-#     |> String.split(~r/\R/)
-#     |> Enum.map(&process_game/1)
-#     |> Enum.sum()
-#     |> IO.inspect(label: "Total Score")
-#   end
-
-#   @doc false
-#   defp process_game(input) do
-#     [game_data | rounds] = String.split(input, ~r{;|:})
-#     game_id = extract_game_id(game_data)
-#     max_blocks = %{"red" => 12, "green" => 13, "blue" => 14}
-
-#     rounds
-#     |> Enum.map(&String.split(&1, ","))
-#     |> List.flatten()
-#     |> Enum.map(&Helper.parse_number_and_color/1)
-#     |> calculate_score()
-#   end
-
-#   @doc false
-#   defp extract_game_id(game_data) do
-#     game_data
-#     |> String.split(" ")
-#     |> List.last()
-#     |> String.to_integer()
-#   end
-
-#   @doc false
-#   defp calculate_score(game) do
-#     colors = ["blue", "red", "green"]
-#     Enum.reduce(colors, 1, fn color, acc ->
-#       max_value = Helper.max_color_value(game, color)
-#       acc * max_value
-#     end)
-#   end
-# end
-
-# # Example usage:
-# GameProcessor.process_game_data("day_2/advent_2.txt")
-
 defmodule Helper do
   def under_max?(list, max_blocks) do
     if length(list) > 2 do
@@ -123,28 +28,10 @@ inputs =
       IO.inspect("Failed to read the file: #{reason}")
   end
 
-# Split the string into the game ID, and the three respective times
-# Get the Game ID from the Array
-
-#  Splice for the remaining three rounds
-# In each round split by the "," and then get the color and number
-#  Compare the number to the Max amount
-
-max_blocks = %{"red" => 12, "green" => 13, "blue" => 14}
-
 reg = ~r/(?=(\d|blue|red|green))/
 
 inputs
 |> Enum.map(fn input ->
-  game_id =
-    input
-    |> String.split(~r{;|:}, trim: true)
-    |> List.first()
-    |> String.split(" ")
-    |> List.last()
-    |> Integer.parse()
-    |> elem(0)
-
   games =
     input
     |> String.split(~r{;|:}, trim: true)
